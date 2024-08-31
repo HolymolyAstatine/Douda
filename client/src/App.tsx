8a257fb71acd595d787972dc12a1c327da37e42e
HolymolyAstatine <kpa7089@gmail.com>
2024-09-01 00:05:26 +0900

    학교코드긁어오기 날먹

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,17 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SchoolSearch from './nice/Components/api';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SchoolSearch />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
